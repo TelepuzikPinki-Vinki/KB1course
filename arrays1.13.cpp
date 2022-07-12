a570af124870362696eb84d7d3522ae8df76eb20
KsuSha <84922299+XsenIva@users.noreply.github.com>
2022-07-12 13:38:42 +0400

    Update and rename массив1. 13.cpp to arrays1.13.cpp

---
+++ arrays1.13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+using namespace std;
+int main()
+{
+    int k = 0, s = 0;
+    int a[10], n;
+    cout << "n="; cin >> n;
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < n; i++) {
+        a[i] = rand() % 15;
+        cout << "a[" << i << "]=" << a[i] << endl;
+
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (((a[i] % 2) == 0) && (a[i] != 0)&& ((i%2)!=0 )) {
+            s += a[i];
+        }
+    }
+
+    if (s != 0)
+        cout << "summa =" << s;
+    else cout << "net dannyih";
+
+    return 0;
+}
