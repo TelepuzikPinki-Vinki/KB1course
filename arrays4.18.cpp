3a65efd8cef86cd4804de8ac844da5b8030b77d4
KsuSha <84922299+XsenIva@users.noreply.github.com>
2022-07-12 13:35:10 +0400

    Update and rename массиыв 4.18.cpp to arrays4.18.cpp

---
+++ arrays4.18.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+	int  n, s;
+	cout << "n = "; cin >> n;
+	int** a = new int* [n];
+	for (int i = 0; i < n; i++)
+		a[i] = new int[n];
+	s = 0;
+		if (n % 2 == 0) {
+			for (int j = n; j > 0; j--)
+			   if (j % 2 == 0) {
+					for (int i = n; i > 0; i--)
+					 {
+							a[i][j] = s;
+							s += 1;
+						}
+				 }
+				else
+					for (int i = 0; i < n; i++) {
+						a[i][j] = s;
+						s += 1;
+					}
+			}
+	   else         for (int j = n; j > 0; j--){
+			             if ((j % 2) != 0) 
+						for (int i = n; i > 0; i--) {
+								a[i][j] = s;
+								s += 1;
+							}
+					     
+				       else
+					   for (int i = 0; i < n; i++) {
+						a[i][j] = s;
+						s += 1;
+					    }
+			}
+	for (int i = 0; i < n; i++, cout << endl)
+		for (int j = 0; j < n; j++) {
+			cout.width(5);
+			cout << a[i][j] << " ";
+		}
+	return 0;
+}
+
